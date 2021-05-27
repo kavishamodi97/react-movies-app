7487df44f04f5df613119835d533d8e375212c2a
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-27 19:29:24 +0530

    Revert "Added Tabs inside modal for Login button"
    This reverts commit 9acb8ebb579d5e498af3e55641009b7fbf995931.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,24 +2,19 @@ import React, { Component } from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
-import Modal from 'react-modal';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Modal from 'react-modal'
 
 class Header extends Component {
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false,
-            value: 0
+            modalIsOpen: false
         };
     }
 
     openModalHandler = () => { this.setState({ modalIsOpen: true }) }
 
     closeModalHandler = () => { this.setState({ modalIsOpen: false }) }
-
-    tabChangeHandler = (event, value) => { this.setState({ value }) };
 
     render() {
         return (
@@ -38,10 +33,6 @@ class Header extends Component {
                     isOpen={this.state.modalIsOpen}
                     contentLabel="Login"
                     onRequestClose={this.closeModalHandler}>
-                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
-                        <Tab label="Login" />
-                        <Tab label="Register" />
-                    </Tabs>
                 </Modal>
             </div>
         );
