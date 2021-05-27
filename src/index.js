2454149ce25e9d483b3138aa8b3a3eafb232cfd4
kavishamodi97 <kavishamodi971@gmail.com>
2021-05-27 16:00:21 +0530

    Added ‘Login’ button on Header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './screens/Home/Home';
+import Home from './screens/home/Home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
